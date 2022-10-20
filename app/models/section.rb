d37bd4ed6d814119b94a24d584232eddbc991f74
Weronika <weronika.piotrowska@tdcm.io>
2022-10-21 01:50:29 +0200

    add name enum to section model

--- app/models/section.rb
+++ app/models/section.rb
@@ -7,4 +7,21 @@ class Section < ApplicationRecord
   has_many :languages, dependent: :destroy
   has_many :references, dependent: :destroy
 
+  enum name: {
+    basic_user: 0,
+    coach: 1,
+    lead: 4,
+    admin: 2,
+    super_admin: 3
+  }
+  NAMES = names.to_h { |k, _v| [k.to_sym, k] }.freeze
+
+  def self.get_names
+    NAMES
+  end
+
+  def self.get_human_names
+    NAMES.to_h { |k, _v| [Section.human_enum_name(:names, k), k] }
+  end
+
 end
