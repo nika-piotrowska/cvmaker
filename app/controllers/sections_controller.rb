2436cc8347bf7fb928eee2b4eda84b3c3e757332
Weronika <weronika.piotrowska@tdcm.io>
2022-10-28 00:28:51 +0200

    add check if section position is consistent

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -21,7 +21,12 @@ class SectionsController < ApplicationController
   def update
     @section = Section.find(params[:id])
     @cv = Cv.find(params[:cv_id])
+    if @section.horizontal_position != sections_params(:section)[:horizontal_position]
+      params[:section][:vertical_position] = @cv.sections.where(horizontal_position: sections_params(:section)[:horizontal_position]).size + 1
+    end
     if @section.update(sections_params(:section))
+      assure_position_consistancy
+      byebug
       respond_to do |format|
         format.js { render 'sections/sections_list.js.erb', layout: false }
       end
@@ -29,6 +34,19 @@ class SectionsController < ApplicationController
   end
 
   private
+
+  def assure_position_consistancy
+    mainBodySections = @cv.sections.where(horizontal_position: Section.horizontal_positions[:main_body])
+    sideBodySections = @cv.sections.where(horizontal_position: Section.horizontal_positions[:side_body])
+    mainBodySections.each.with_index(1) do | section, index |
+      section.vertical_position = index
+      section.save
+    end
+    sideBodySections.each.with_index(1) do | section, index |
+      section.vertical_position = index
+      section.save
+    end
+  end
 
   def sections_params(required_symbol)
     params.require(required_symbol).permit(
